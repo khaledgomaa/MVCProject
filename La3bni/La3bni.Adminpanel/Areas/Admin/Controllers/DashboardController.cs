538094d7c3dc4b67817ef03806b56dbf2c24127e
khaledgomaa <khaledgomaa670@gmail.com>
2021-02-17 12:56:49 +0200

    Added adminPanel to La3bni solution

---
+++ La3bni/La3bni.Adminpanel/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace La3bni.Adminpanel.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Dashboard")]
+    public class DashboardController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
