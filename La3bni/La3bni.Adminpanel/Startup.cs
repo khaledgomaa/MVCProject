b2e1bd4503c1a08f3125d9353ae59e09f547ef47
khaledgomaa <khaledgomaa670@gmail.com>
2021-02-26 18:43:51 +0200

    saosoda

--- La3bni/La3bni.Adminpanel/Startup.cs
+++ La3bni/La3bni.Adminpanel/Startup.cs
@@ -23,13 +23,12 @@ namespace La3bni.Adminpanel
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IEmailRepository, EmailRepository>();
             services.AddControllersWithViews();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             services.AddDbContext<La3bniContext>(options => options.UseSqlServer(configuration.GetConnectionString("La3bniCon")));
-
-            services.AddScoped<IEmailRepository, EmailRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
